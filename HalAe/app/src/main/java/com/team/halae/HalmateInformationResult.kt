246517ec15cb478d90aaaa1c2671fd49964061de
choigone <goun4439@gmail.com>
2018-11-06 15:13:05 +0900

    고운 통신

---
+++ HalAe/app/src/main/java/com/team/halae/HalmateInformationResult.kt
@@ -0,0 +1,10 @@
+package com.team.halae
+
+data class HalmateInformationResult(
+        var hal_name : String,
+        var hal_gender : Int,
+        var hal_img : String,
+        var hal_address : String,
+        var hal_phone : String,
+        var vol_cnt : Int
+)
---
+++ HalAe/app/src/main/java/com/team/halae/HalmateScheuleInfoResponse.kt
@@ -0,0 +1,6 @@
+package com.team.halae
+
+data class HalmateScheuleInfoResponse(
+        var message : String,
+        var data : ArrayList<HalmateScheduleData>
+)
