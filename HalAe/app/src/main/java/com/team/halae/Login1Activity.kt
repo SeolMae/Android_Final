173dcd52d848d0d953748ee012f44dd669926902
신예지 <gjsld1@naver.com>
2018-11-16 13:21:16 +0900

    꺄

--- HalAe/app/src/main/java/com/team/halae/Login1Activity.kt
+++ HalAe/app/src/main/java/com/team/halae/Login1Activity.kt
@@ -1,12 +1,30 @@
 package com.team.halae
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import kotlinx.android.synthetic.main.activity_login1.*
 
 class Login1Activity : AppCompatActivity() {
+
+    var name : String? = null
+    var phonenumber : String? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login1)
+
+        name = login1_name.getText().toString()
+        phonenumber = login1_nubmer.getText().toString()
+
+        next_login2.setOnClickListener{
+
+            var intent = Intent(this, LoginActivity2::class.java)
+            intent.putExtra("usr_name", name)
+            intent.putExtra("usr_phone", phonenumber)
+            startActivity(intent)
+
+        }
     }
 }
